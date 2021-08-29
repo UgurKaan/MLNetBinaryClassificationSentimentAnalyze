0e1a54fa111dc158060ff69d2bd78709c9adb33b
tevhidi <80151492+tevhidi@users.noreply.github.com>
2021-08-29 20:05:50 +0300

    ML.Net Binary Classification
    ML.Net Sentiment Analyze with Binary Classification
    AutoML

---
+++ BinaryCl1/Models/SentimentPrediction.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryCl1.Models
+{
+    class SentimentPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        public float Score { get; set; }
+    }
+}
---
+++ BinaryCl1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.ML;
+using Microsoft.ML.AutoML;
+using System;
+
+namespace BinaryCl1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            var mlContext = new MLContext();
+
+            var trainingData = mlContext.Data.LoadFromTextFile<Models.SentimentIssue>("dataset/trainingdata.tsv", hasHeader: true);
+            var testData = mlContext.Data.LoadFromTextFile<Models.SentimentIssue>("dataset/testData.tsv", hasHeader: true);
+
+            ExperimentResult<Microsoft.ML.Data.BinaryClassificationMetrics> experimentResult = mlContext.Auto()
+                .CreateBinaryClassificationExperiment(10).Execute(trainingData);
+            var model = experimentResult.BestRun.Model;
+
+            var predictions = model.Transform(testData);
+            var metrics = mlContext.BinaryClassification.EvaluateNonCalibrated(data: predictions, scoreColumnName: "Score");
+            mlContext.Model.Save(model, trainingData.Schema, "Model.zip");
+        */
+            var mlContext = new MLContext();
+            DataViewSchema model;
+           
+            var transformer = mlContext.Model.Load("Model.zip",out model);
+            PredictionEngine<Models.SentimentIssue, Models.SentimentPrediction> predictionEngine =
+                mlContext.Model.CreatePredictionEngine<Models.SentimentIssue, Models.SentimentPrediction>(transformer);
+
+           var prediction  = predictionEngine.Predict(new Models.SentimentIssue() { 
+                Text="I hate this worst application "
+            });
+            Console.WriteLine("My Prediction:" + prediction.Prediction);
+        }
+    }
+}
